d0cf235f73366868857362bc036539a5f702f3ce
marcobusacca <marcobusacca01@gmail.com>
2023-12-13 11:38:36 +0100

    [FEAT] created class "PizzaRestController"

---
+++ src/main/java/org/java/spring/restcontroller/PizzaRestController.java
@@ -0,0 +1,12 @@
+package org.java.spring.restcontroller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1.0/pizzas")
+public class PizzaRestController {
+
+}
