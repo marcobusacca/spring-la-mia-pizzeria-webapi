d53dce16dae9f4baf779b4e1eb7a1d8e25b05389
marcobusacca <marcobusacca01@gmail.com>
2023-12-13 12:56:33 +0100

    [FEAT] PizzaRestController method "update"

--- src/main/java/org/java/spring/restcontroller/PizzaRestController.java
+++ src/main/java/org/java/spring/restcontroller/PizzaRestController.java
@@ -1,6 +1,5 @@
 package org.java.spring.restcontroller;
 
-import java.awt.print.Book;
 import java.util.List;
 
 import org.java.spring.db.pojo.Pizza;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,6 +48,21 @@ public class PizzaRestController {
 	
 	@PostMapping
 	public ResponseEntity<Pizza> create(@RequestBody Pizza pizza) {
+
+		pizzaService.save(pizza);
+
+		return new ResponseEntity<>(pizza, HttpStatus.OK);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Pizza> update(@PathVariable int id, @RequestBody Pizza newPizza) {
+
+		Pizza pizza = pizzaService.findById(id);
+
+		pizza.setNome(newPizza.getNome());
+		pizza.setDescrizione(newPizza.getDescrizione());
+		pizza.setUrl_foto(newPizza.getUrl_foto());
+		pizza.setPrezzo(newPizza.getPrezzo());
 
 		pizzaService.save(pizza);
 
