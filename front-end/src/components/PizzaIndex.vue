6fb7031a9fd2ba745a77c47f9fd27c4349d6de71
marcobusacca <marcobusacca01@gmail.com>
2023-12-14 17:58:45 +0100

    Esercizio Completato

--- front-end/src/components/PizzaIndex.vue
+++ front-end/src/components/PizzaIndex.vue
@@ -1,5 +1,7 @@
 <script setup>
 // IMPORT LIBS
+import { ref } from 'vue';
+import { computed } from 'vue';
 import { defineProps } from 'vue';
 
 // EMITS
@@ -12,9 +14,21 @@ const props = defineProps({
         required: true
     }
 });
+
+// DATA
+const nomeFilter = ref('');
+
+// COMPUTED PROPERTY
+const filteredPizzas = computed(() => {
+    const searchTerm = nomeFilter.value.toLowerCase().trim();
+    return searchTerm ? props.pizzas.filter(pizza => pizza.nome.toLowerCase().includes(searchTerm)) : props.pizzas;
+});
 </script>
 
 <template>
+    <form class="index-filter-form py-5" @submit.prevent="filterPizzas">
+        <input type="text" placeholder="Filtra le pizze per nome" class="form-control d-inline-block" v-model="nomeFilter">
+    </form>
     <table class="table table-light table-hover index-table">
         <thead>
             <tr>
@@ -24,7 +38,7 @@ const props = defineProps({
             </tr>
         </thead>
         <tbody>
-            <tr v-for="pizza in pizzas" :key="pizza.id">
+            <tr v-for="pizza in filteredPizzas" :key="pizza.id">
                 <th scope="row" v-text="pizza.id"></th>
                 <td v-text="pizza.nome"></td>
                 <td>
@@ -44,6 +58,18 @@ const props = defineProps({
 
 <style lang="scss" scoped>
 @use '../styles/generals.scss' as *;
+
+.index-filter-form {
+    display: flex;
+    justify-content: center;
+    background-color: white;
+
+    input {
+        width: 75%;
+        color: black !important;
+        border: 1px solid black;
+    }
+}
 
 .index-table {
     width: 50vw !important;
