4f24eaea3e973260794219d4a66831d3b2bfae71
marcobusacca <marcobusacca01@gmail.com>
2023-12-14 13:10:44 +0100

    [BACK-END] setup CorsSettings in AuthConf.java

--- src/main/java/org/java/spring/restcontroller/PizzaRestController.java
+++ src/main/java/org/java/spring/restcontroller/PizzaRestController.java
@@ -7,7 +7,6 @@ import org.java.spring.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/api/v1.0/pizzas")
 public class PizzaRestController {
 
